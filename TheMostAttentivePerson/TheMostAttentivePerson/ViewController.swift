b7a8d66f9b6204b89669f6c70752fb79366a407c
biyenka <135021588+biyenka@users.noreply.github.com>
2023-06-25 19:50:32 +0200

    Init

---
+++ TheMostAttentivePerson/TheMostAttentivePerson/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheMostAttentivePerson
+//
+//  Created by Kate Bienko on 25/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
