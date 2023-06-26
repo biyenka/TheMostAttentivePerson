77e0ea59046c16ba9ae8a326e6d077ab66875ada
biyenka <135021588+biyenka@users.noreply.github.com>
2023-06-26 16:46:45 +0200

    Added storyboard

--- TheMostAttentivePerson/TheMostAttentivePerson/ViewController.swift
+++ TheMostAttentivePerson/TheMostAttentivePerson/ViewController.swift
@@ -1,19 +1,27 @@
-//
-//  ViewController.swift
-//  TheMostAttentivePerson
-//
-//  Created by Kate Bienko on 25/06/2023.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet private weak var fixButton: UIButton!
+    @IBOutlet private weak var background: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+         
+        background.backgroundColor = UIColor(patternImage: UIImage(named: "1.jpg")!)
+        background.contentMode = .scaleToFill
+        
+        _ = Timer.scheduledTimer(withTimeInterval: 4.0, repeats: false) { [self] timer in
+            background.backgroundColor = UIColor(patternImage: UIImage(named: "1_2.jpg")!)
+            background.contentMode = .scaleToFill
+        }
     }
-
-
+    
+    @IBAction func tapFixButton(_ sender: Any) {
+        if background.backgroundColor == UIColor(patternImage: UIImage(named: "1_2.jpg")!) {
+            background.backgroundColor = UIColor(patternImage: UIImage(named: "1.jpg")!)
+            background.contentMode = .scaleToFill
+        }
+    }
 }
 
